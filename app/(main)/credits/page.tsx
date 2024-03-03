5e0716a7acfea76ded496e2711a045bbd1b30e28
Dan <daniel.speranskiy@live.com>
2024-03-03 09:29:45 +0400

    Adding in stripe integration

--- app/(main)/credits/page.tsx
+++ app/(main)/credits/page.tsx
@@ -1,9 +1,167 @@
-import React from 'react'
+import { SignedIn, auth } from "@clerk/nextjs";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const CreditsPage = () => {
+import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
+import { getUserById } from "@/lib/actions/user.actions";
+import Checkout from "@/components/Checkout";
+const plans = [
+  {
+    _id: 1,
+    name: "Free",
+    icon: "/plan.svg",
+    price: 0,
+    credits: 20,
+    inclusions: [
+      {
+        label: "20 Free Credits",
+        isIncluded: true,
+      },
+      {
+        label: "Basic Access to Services",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Customer Support",
+        isIncluded: false,
+      },
+      {
+        label: "Priority Updates",
+        isIncluded: false,
+      },
+    ],
+  },
+  {
+    _id: 2,
+    name: "Pro Package",
+    icon: "/plan.svg",
+    price: 40,
+    credits: 120,
+    inclusions: [
+      {
+        label: "120 Credits",
+        isIncluded: true,
+      },
+      {
+        label: "Full Access to Services",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Customer Support",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Updates",
+        isIncluded: false,
+      },
+    ],
+  },
+  {
+    _id: 3,
+    name: "Premium Package",
+    icon: "/plan.svg",
+    price: 199,
+    credits: 2000,
+    inclusions: [
+      {
+        label: "2000 Credits",
+        isIncluded: true,
+      },
+      {
+        label: "Full Access to Services",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Customer Support",
+        isIncluded: true,
+      },
+      {
+        label: "Priority Updates",
+        isIncluded: true,
+      },
+    ],
+  },
+];
+const Credits = async () => {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+
   return (
-    <div>CreditsPage</div>
-  )
-}
+    <>
+      <Header
+        title="Buy Credits"
+        subtitle="Choose a credit package that suits your needs!"
+      />
 
-export default CreditsPage
+      <section>
+        <ul className="mt-11 grid grid-cols-1 gap-5 sm:grid-cols-2 md:gap-9 xl:grid-cols-3">
+          {plans.map((plan) => (
+            <li
+              key={plan.name}
+              className="w-full rounded-[16px] border-2 border-purple-200/20 bg-white p-8 shadow-xl shadow-purple-200/20 lg:max-w-none"
+            >
+              <div className="flex justify-center items-center flex-col gap-3">
+                <Image src={plan.icon} alt="check" width={50} height={50} />
+                <p className="font-semibold text-[20px] leading-[140%] mt-2 text-purple-500">
+                  {plan.name}
+                </p>
+                <p className="text-[36px] font-semibold sm:text-[44px] leading-[120%] sm:leading-[56px] text-blue-900">
+                  ${plan.price}
+                </p>
+                <p className="font-normal text-[16px] leading-[140%]">
+                  {plan.credits} Credits
+                </p>
+              </div>
+
+              {/* Inclusions */}
+              <ul className="flex flex-col gap-5 py-9">
+                {plan.inclusions.map((inclusion) => (
+                  <li
+                    key={plan.name + inclusion.label}
+                    className="flex items-center gap-4"
+                  >
+                    <Image
+                      src={`/${
+                        inclusion.isIncluded ? "check.svg" : "cross.svg"
+                      }`}
+                      alt="check"
+                      width={24}
+                      height={24}
+                    />
+                    <p className="font-normal text-[16px] leading-[140%]">
+                      {inclusion.label}
+                    </p>
+                  </li>
+                ))}
+              </ul>
+
+              {plan.name === "Free" ? (
+                <Button
+                  variant="outline"
+                  className="w-full rounded-full bg-purple-300 bg-cover text-purple-500 hover:text-purple-500"
+                >
+                  Free Consumable
+                </Button>
+              ) : (
+                <SignedIn>
+                  <Checkout
+                    plan={plan.name}
+                    amount={plan.price}
+                    credits={plan.credits}
+                    buyerId={user._id}
+                  />
+                </SignedIn>
+              )}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default Credits;
